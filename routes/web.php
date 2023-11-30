49dda9eb0fcaaa92a4d4b74ce59d022fe77634a9
programinglive <75464690+programinglive@users.noreply.github.com>
2023-11-30 10:32:01 +0700

    chore(starter-kits): add laravel breeze

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Livewire\LandingPage;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,4 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', LandingPage::class)->name('landing-page');
+Route::view('/', 'welcome');
+
+Route::view('dashboard', 'dashboard')
+    ->middleware(['auth', 'verified'])
+    ->name('dashboard');
+
+Route::view('profile', 'profile')
+    ->middleware(['auth'])
+    ->name('profile');
+
+require __DIR__.'/auth.php';
